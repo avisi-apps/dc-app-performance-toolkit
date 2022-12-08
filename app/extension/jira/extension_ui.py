e30e4b8d00bb1a8d2f1b77a7132a954c38a61b2b
Mitchel Kuijpers <mitchelkuijpers@gmail.com>
2022-12-08 11:29:40 +0100

    Setup manage custom field custom action

--- app/extension/jira/extension_ui.py
+++ app/extension/jira/extension_ui.py
@@ -18,39 +18,33 @@ def app_specific_action(webdriver, datasets):
     # NOTE: If app_specific_action is running as specific user, make sure that app_specific_action is running
     # just before test_2_selenium_z_log_out action
     #
-    # @print_timing("selenium_app_specific_user_login")
-    # def measure():
-    #     def app_specific_user_login(username='admin', password='admin'):
-    #         login_page = Login(webdriver)
-    #         login_page.delete_all_cookies()
-    #         login_page.go_to()
-    #         login_page.set_credentials(username=username, password=password)
-    #         if login_page.is_first_login():
-    #             login_page.first_login_setup()
-    #         if login_page.is_first_login_second_page():
-    #             login_page.first_login_second_page_setup()
-    #         login_page.wait_for_page_loaded()
-    #     app_specific_user_login(username='admin', password='admin')
-    # measure()
+    @print_timing("selenium_app_specific_user_login")
+    def measure():
+        def app_specific_user_login(username='admin', password='admin'):
+            login_page = Login(webdriver)
+            login_page.delete_all_cookies()
+            login_page.go_to()
+            login_page.set_credentials(username=username, password=password)
+            if login_page.is_first_login():
+                login_page.first_login_setup()
+            if login_page.is_first_login_second_page():
+                login_page.first_login_second_page_setup()
+            login_page.wait_for_page_loaded()
+
+        app_specific_user_login(username='admin', password='admin')
+
+    measure()
 
     @print_timing("selenium_app_custom_action")
     def measure():
-        @print_timing("selenium_app_custom_action:undo-transition")
+        @print_timing("selenium_app_custom_action:view_manage_custom_field_values_overview")
         def sub_measure():
-            page.go_to_url(f"{JIRA_SETTINGS.server_url}/browse/{issue_key}")
-            # Click on Start progress button in issue to transition
-            page.wait_until_clickable((By.ID, "action_id_11")).click()
+            page.go_to_url(
+                f"{JIRA_SETTINGS.server_url}/secure/ProjectConfigureEditCustomFieldValues.jspa?projectKey=UT")
 
             page.wait_until_any_ec_text_presented_in_el(
-                selector_text_list=[((By.CSS_SELECTOR, "#status-val span"), 'IN PROGRESS')])
-
-            page.wait_until_clickable((By.ID, "opsbar-operations_more")).click()
-
-            page.wait_until_clickable((By.CSS_SELECTOR, "#undo-transition-link a")).click()
-
-            page.wait_until_clickable((By.CSS_SELECTOR, "#undo-transition-dialog .submit-button")).click()
-
-            page.wait_until_any_ec_text_presented_in_el(
-                selector_text_list=[((By.CSS_SELECTOR, "#status-val span"), 'OPEN')])
+                selector_text_list=[
+                    ((By.CSS_SELECTOR, ".atlas-crm-kit_ui_components_table_Table__table td"), 'Abstruse'),
+                ])
         sub_measure()
     measure()
