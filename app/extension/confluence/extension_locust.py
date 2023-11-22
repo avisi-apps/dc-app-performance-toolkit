6a6dc991f8e771bb0ccd642cd402d933588925fe
Mitchel Kuijpers <mitchelkuijpers@gmail.com>
2023-11-22 14:09:18 +0100

    Add locusts load tests

--- app/extension/confluence/extension_locust.py
+++ app/extension/confluence/extension_locust.py
@@ -4,29 +4,16 @@ from locustio.common_utils import init_logger, confluence_measure, run_as_specif
 logger = init_logger(app_type='confluence')
 
 
-@confluence_measure("locust_app_specific_action")
-# WebSudo is a feature that enhances security by requiring administrators to re-authenticate before
-# accessing administrative functions within Atlassian applications.
-# do_websudo=True requires user administrative rights, otherwise requests fail.
-#@run_as_specific_user(username='admin', password='admin', do_websudo=False)  # run as specific user
+@confluence_measure("locust_list_servers_action")
+@run_as_specific_user(username='admin', password='admin')  # run as specific user
 def app_specific_action(locust):
-    r = locust.get('/app/get_endpoint', catch_response=True)  # call app-specific GET endpoint
+    r = locust.get('/rest/git-plugin/1.0/servers', catch_response=True)  # call app-specific GET endpoint
     content = r.content.decode('utf-8')   # decode response content
 
-    token_pattern_example = '"token":"(.+?)"'
-    id_pattern_example = '"id":"(.+?)"'
-    token = re.findall(token_pattern_example, content)  # get TOKEN from response using regexp
-    id = re.findall(id_pattern_example, content)    # get ID from response using regexp
+    if 'Github' not in content:
+        logger.error(f"'Github' was not found in {content}")
+    assert 'Github' in content  # assert specific string in response content
 
-    logger.locust_info(f'token: {token}, id: {id}')  # log info for debug when verbose is true in confluence.yml file
-    if 'assertion string' not in content:
-        logger.error(f"'assertion string' was not found in {content}")
-    assert 'assertion string' in content  # assert specific string in response content
-
-    body = {"id": id, "token": token}  # include parsed variables to POST request body
-    headers = {'content-type': 'application/json'}
-    r = locust.post('/app/post_endpoint', body, headers, catch_response=True)  # call app-specific POST endpoint
-    content = r.content.decode('utf-8')
-    if 'assertion string after successful POST request' not in content:
-        logger.error(f"'assertion string after successful POST request' was not found in {content}")
-    assert 'assertion string after successful POST request' in content  # assertion after POST request
+    if 'Bitbucket' not in content:
+        logger.error(f"'Bitbucket' was not found in {content}")
+    assert 'Bitbucket' in content  # assert specific string in response content
