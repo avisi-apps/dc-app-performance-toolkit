37bc26915d27bc05091271edf522196b26723e54
Jannick Joosten <j.joosten@avisi.nl>
2024-11-12 10:54:08 +0100

    Add locust test

--- app/extension/jira/extension_locust.py
+++ app/extension/jira/extension_locust.py
@@ -1,4 +1,3 @@
-import re
 from locustio.common_utils import init_logger, jira_measure, run_as_specific_user  # noqa F401
 
 logger = init_logger(app_type='jira')
@@ -9,24 +8,11 @@ logger = init_logger(app_type='jira')
 # accessing administrative functions within Atlassian applications.
 # do_websudo=True requires user administrative rights, otherwise requests fail.
 #@run_as_specific_user(username='admin', password='admin', do_websudo=False)  # run as specific user
+@run_as_specific_user(username='admin', password='admin')  # run as specific user
 def app_specific_action(locust):
-    r = locust.get('/app/get_endpoint', catch_response=True)  # call app-specific GET endpoint
-    content = r.content.decode('utf-8')   # decode response content
+    r = locust.get('/rest/editcustomfieldvalues/latest/customfields', catch_response=True)  # call app-specific GET endpoint
+    json = r.json()
 
-    token_pattern_example = '"token":"(.+?)"'
-    id_pattern_example = '"id":"(.+?)"'
-    token = re.findall(token_pattern_example, content)  # get TOKEN from response using regexp
-    id = re.findall(id_pattern_example, content)    # get ID from response using regexp
+    assert r.status_code == 200
 
-    logger.locust_info(f'token: {token}, id: {id}')  # log info for debug when verbose is true in jira.yml file
-    if 'assertion string' not in content:
-        logger.error(f"'assertion string' was not found in {content}")
-    assert 'assertion string' in content  # assert specific string in response content
-
-    body = {"id": id, "token": token}  # include parsed variables to POST request body
-    headers = {'content-type': 'application/json'}
-    r = locust.post('/app/post_endpoint', body, headers, catch_response=True)  # call app-specific POST endpoint
-    content = r.content.decode('utf-8')
-    if 'assertion string after successful POST request' not in content:
-        logger.error(f"'assertion string after successful POST request' was not found in {content}")
-    assert 'assertion string after successful POST request' in content  # assertion after POST request
+    assert json[0]['name'] == 'Abstruse'
